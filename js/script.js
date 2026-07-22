de9bfd52a7f0842b5a5d506febc70285e4b65973
Lavoisier-1 <mivanapoli@gmail.com>
2026-07-22 17:54:01 -0300

    Implement 'Back to Top' button functionality
    Add functionality for a 'Back to Top' button that appears after scrolling down.

--- js/script.js
+++ js/script.js
@@ -83,3 +83,19 @@ window.addEventListener("load", () => {
 // Año automático
 document.querySelector("footer p").textContent =
 `© ${new Date().getFullYear()} Clases Lavoisier`;
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 300) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+});
+
+topBtn.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
